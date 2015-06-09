c1cbe892675b7ab2d8193ea8610e8e18a445879c
elkyur <elkyur@hotmail.com>
2015-06-09 20:22:10 +0300

    muutama pieni lisäys

---
+++ Actual Gradle Project/core/src/Battle/Objects/MoverMem/MoverMemory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Battle.Objects.MoverMem;
+
+import Algorithms.GraphAlgorithms.BNode;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ *
+ * @author elkyur
+ */
+public class MoverMemory {
+    
+    HashSet<BNode> greenpoints;
+    HashMap<BNode, YellowPoint> yellowpoints;
+    
+    public MoverMemory()
+    {
+    
+    
+    }
+    
+    
+    
+}
